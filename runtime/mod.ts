f53f3ea7dbde466279a4188fe5ad8ce4a2c8c0c8
divy-work <dj.srivastava23@gmail.com>
2020-08-19 12:06:05 +0530

    chores: improvements

--- runtime/mod.ts
+++ runtime/mod.ts
@@ -1,18 +1,25 @@
 import Iroh from "./runtime.js";
 
-export async function runtimeAnalyze(code: string, rules: Function[]) {
+export async function runtimeAnalyze(code: string, rules: Function[]): Promise<any> {
   let stage = new Iroh.Stage(code);
   let listener = stage.addListener(Iroh.CALL);
-
-  listener.on("before", (e: any) => {
-    if (rules.includes(e.call)) {
-      console.log(e);
-      let args = e.arguments;
-      // Makes evaluation safe
-      e.call = () => code;
-    }
-  });
-
-  // ;) Don't worry mate, it is safe
-  eval(stage.script);
+  let program = stage.addListener(Iroh.PROGRAM);
+  let diagnostics: any[] = [];
+  return new Promise((res, rej) => {
+      listener.on("before", (e: any) => {
+        if (rules.includes(e.call)) {
+          // Makes evaluation safe
+          e.call = () => code;
+          diagnostics.push(e);
+        }
+        return;
+      });
+    // program
+    program
+    .on("leave", (e: any) => {
+      res(diagnostics);
+    });
+    // ;) Don't worry mate, it is safe
+    eval(stage.script);
+  })
 }
