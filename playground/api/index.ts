6895730eb2548519db23d372ec637591befa0803
divy-work <dj.srivastava23@gmail.com>
2020-08-22 18:14:50 +0530

    feat: experimental analytics

--- playground/api/index.ts
+++ playground/api/index.ts
@@ -1,5 +1,11 @@
 import { ServerRequest } from 'https://deno.land/std@0.66.0/http/server.ts';
+import { analyze, Diagnostics } from "https://raw.github.com/nestdotland/analyzer/master/deno/mod.ts";
 
 export default async (req: ServerRequest) => {
-	req.respond({ body: `Hello, from Deno v${Deno.version.deno}!` });
+    const buf: Uint8Array = await Deno.readAll(req.body);
+    const src: string = new TextDecoder().decode(buf);
+    let diagnostics: Diagnostics = await analyze(src, {
+        runtime: true // TODO(@divy-work): set this to false unless we are sure that evaluation is safe.
+    });
+	req.respond({ body: JSON.stringify(diagnostics) });
 };
