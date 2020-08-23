3d3f216dcd191d80674d473b6b7139bec623d7c1
divy-work <dj.srivastava23@gmail.com>
2020-08-23 21:34:48 +0530

    feat: working runtime

--- examples/deno/mod.ts
+++ examples/deno/mod.ts
@@ -3,7 +3,9 @@ import { analyze } from "../../deno/mod.ts";
 let diagnostics = await analyze(`
 Deno.run({ cmd: "echo 2".split(" ")});
 console.log("Something else");
-`);
+`, {
+    runtime: true
+});
 
 console.log("===Diagnostics===");
 console.log(diagnostics);
--- playground/api/index.ts
+++ playground/api/index.ts
@@ -7,10 +7,13 @@ import {
 export default async (req: ServerRequest) => {
   const buf: Uint8Array = await Deno.readAll(req.body);
   const src: string = new TextDecoder().decode(buf);
-
-  console.log(src);
-  let diagnostics: Diagnostics = await analyze(src, {
-    runtime: true, // TODO(@divy-work): set this to false unless we are sure that evaluation is safe.
-  });
-  req.respond({ body: JSON.stringify(diagnostics) });
+  try {
+    let diagnostics: Diagnostics = await analyze(src, {
+      runtime: true, // TODO(@divy-work): set this to false unless we are sure that evaluation is safe.
+    });
+    await req.respond({ body: JSON.stringify(diagnostics) });
+  } catch (error) {
+    await req.respond({ body: JSON.stringify({ error }) });
+    throw new Error("Oops...");
+  }
 };
